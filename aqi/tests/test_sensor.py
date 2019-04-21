58eefdf6581dbcfc7994fe85f52c1794d3bd3afc
Marcus Lugg <marcuslugg@googlemail.com>
2019-04-21 17:03:44 +0100

    TST: Add another test of saving to file

--- aqi/tests/test_sensor.py
+++ aqi/tests/test_sensor.py
@@ -7,6 +7,25 @@ from aqi.sensor import AirQualitySensor
 
 class TestAirQualitySensor:
 
+    test_filename = os.path.join(__package_root__, 'tests', 'test_file.json')
+
+    def test_data_is_saved_when_file_does_not_already_exist(self):
+        """ Test data is saved when the file does not already exist.
+
+        :raise AssertionError:
+        :return None:
+        """
+        sensor = AirQualitySensor(mock=True)
+        sensor.readings = [{'readings': 'here'}, {'another_one': 'there'}]
+        sensor.save_to_file(self.test_filename)
+
+        with open(self.test_filename, 'r') as f:
+            data = json.load(f)
+
+        assert data == sensor.readings
+
+        os.remove(self.test_filename)
+
     def test_data_is_appended_to_existing_data_in_file(self):
         """ Test data is appended to existing data in file.
 
@@ -15,18 +34,16 @@ class TestAirQualitySensor:
         """
         existing_readings = [{'hello': 'world'}, {'this is': 'a reading'}]
 
-        test_filename = os.path.join(__package_root__, 'tests', 'test_file.json')
-
-        with open(test_filename, 'w') as f:
+        with open(self.test_filename, 'w') as f:
             json.dump(existing_readings, f)
 
         sensor = AirQualitySensor(mock = True)
         sensor.readings = [{'more_readings': 'here'}, {'another_one': 'there'}]
-        sensor.save_to_file(test_filename)
+        sensor.save_to_file(self.test_filename)
         
-        with open(test_filename, 'r') as f:
+        with open(self.test_filename, 'r') as f:
             data = json.load(f)
 
         assert data == existing_readings + sensor.readings
 
-        os.remove(test_filename)
+        os.remove(self.test_filename)
