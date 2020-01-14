183f4e6d9a35515cb6f1c8d42645a8b2fff9fb69
ugoogalizer <signup@mattcurtis.id.au>
2020-01-14 21:14:11 +1100

    Starting work on making x axis linear

--- html/plot.js
+++ html/plot.js
@@ -1,8 +1,6 @@
 //Plot.ly Javascript plot used to display latest measurements
 //Original sounced from "aqi" fork: https://github.com/jtme/aqi
 
-
-
 var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
@@ -11,37 +9,28 @@ xmlhttp.onreadystatechange = function() {
     }
 };
 
-//xmlhttp.open("GET", "http://192.168.0.170/aqi.json", true);
 xmlhttp.open("GET", "aqi.json", true);
 xmlhttp.send();
 
 
-//<script type="text/javascript">
-//function read_json() {
-//    $.getJSON("/aqi.json", function(data) {
-//        alert("My data: " + data["mydata"]);
-//        $.each(data["prime"], function(idx,prime) {
-//            alert("Prime number: " + prime);
-//        });
-//    });
-//}
-//    </script>
 
 function PlotGraph(data) {
   let trace1 = {
     x: [],
     y: [],
-    mode: "lines"
+    //mode: "lines"
+    type: "scatter"
   };
   let trace2 = {
     x: [],
     y: [],
-    mode: "lines"
+    //mode: "lines"
+    type: "scatter"
   };
     data.forEach(function(val) {
-    trace1.x.push(val["time"]);
+    trace1.x.push(Date.parse(val["time"]));
     trace1.y.push(val["pm25"]);
-    trace2.x.push(val["time"]);
+    trace2.x.push(Date.parse(val["time"]));
     trace2.y.push(val["pm10"]);
     });
   Plotly.newPlot('AQIplot', [trace1, trace2]);
