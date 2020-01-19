1909ce5bc02a5b36784722e4630a7a08d1badae3
ugoogalizer <signup@mattcurtis.id.au>
2020-01-19 16:56:36 +1100

    Merging Cam's display changes

--- python/aqi-display.py
+++ python/aqi-display.py
@@ -30,7 +30,7 @@
 # This example is for use on (Linux) computers that are using CPython with
 # Adafruit Blinka to support CircuitPython libraries. CircuitPython does
 # not support PIL/pillow (python imaging library)!
- 
+import sys
 import time
 import subprocess
  
@@ -42,103 +42,131 @@ import atexit
 
 import json
  
+from watchdog.observers import Observer
+from watchdog.events import PatternMatchingEventHandler
  
-# Create the I2C interface.
-i2c = busio.I2C(SCL, SDA)
- 
-# Create the SSD1306 OLED class.
-# The first two parameters are the pixel width and pixel height.  Change these
-# to the right size for your display!
-disp = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c)
- 
-# Clear display.
-disp.fill(0)
-disp.show()
- 
-# Create blank image for drawing.
-# Make sure to create image with mode '1' for 1-bit color.
-width = disp.width
-height = disp.height
-image = Image.new('1', (width, height))
- 
-# Get drawing object to draw on image.
-draw = ImageDraw.Draw(image)
- 
-# Draw a black filled box to clear the image.
-draw.rectangle((0, 0, width, height), outline=0, fill=0)
- 
-# Draw some shapes.
-# First define some constants to allow easy resizing of shapes.
-padding = -2
-top = padding
-bottom = height-padding
-# Move left to right keeping track of the current x position for drawing shapes.
-x = 0
- 
- 
-# Load default font.
-font = ImageFont.load_default()
- 
-# Alternatively load a TTF font.  Make sure the .ttf font file is in the
-# same directory as the python script!
-# Some other nice fonts to try: http://www.dafont.com/bitmap.php
-#font = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf', 9)
 
-#function to clear the screen on exit
-def disableDisplay():
-    disp.poweroff()
-atexit.register(disableDisplay)
+def on_created(event):
+    print(f"hey, {event.src_path} has been created!")
+    redraw()
 
-while True:
- 
+def on_deleted(event):
+    print(f"what the f**k! Someone deleted {event.src_path}!")
+
+def on_modified(event):
+    print(f"hey buddy, {event.src_path} has been modified")
+    redraw()
+
+def on_moved(event):
+    print(f"ok ok ok, someone moved {event.src_path} to {event.dest_path}")
+
+def redraw():
     # Draw a black filled box to clear the image.
     draw.rectangle((0, 0, width, height), outline=0, fill=0)
- 
-    # Shell scripts for system monitoring from here:
-    # https://unix.stackexchange.com/questions/119126/command-to-display-memory-usage-disk-usage-and-cpu-load
-    #cmd = "hostname -I | cut -d\' \' -f1"
-    #IP = subprocess.check_output(cmd, shell=True).decode("utf-8")
-    #cmd = "top -bn1 | grep load | awk '{printf \"CPU Load: %.2f\", $(NF-2)}'"
-    #CPU = subprocess.check_output(cmd, shell=True).decode("utf-8")
-    #cmd = "free -m | awk 'NR==2{printf \"Mem: %s/%s MB  %.2f%%\", $3,$2,$3*100/$2 }'"
-    #MemUsage = subprocess.check_output(cmd, shell=True).decode("utf-8")
-    #cmd = "df -h | awk '$NF==\"/\"{printf \"Disk: %d/%d GB  %s\", $3,$2,$5}'"
-    #Disk = subprocess.check_output(cmd, shell=True).decode("utf-8")
 
-    #Crappy way to extract from json file
-    #aqiHeader = "Air Quality Sensor"
-    #cmd = "tail -n 1 /var/www/html/aqi.json | awk '{print \"\",$6,$7}'"
-    #aqiDateTime = subprocess.check_output(cmd, shell=True).decode("utf-8")
-    #cmd = "tail -n 1 /var/www/html/aqi.json | awk '{print \"PM10:\",$4}'"
-    #aqipm10 = subprocess.check_output(cmd, shell=True).decode("utf-8")
-    #cmd = "tail -n 1 /var/www/html/aqi.json | awk '{print \"PM2.5:\",$2}'"
-    #aqipm25 = subprocess.check_output(cmd, shell=True).decode("utf-8")
+    try:
+        #Proper way to extract from json file by reading json structure
+        with open("/var/www/html/aqi.json", "r") as read_file:
+            measurementdata = json.load(read_file)
+            measurement = measurementdata[-1]
+            #print(measurement)
+            aqipm10 = "PM10: " + str(measurement['pm10'])
+            aqipm25 = "PM2.5: " + str(measurement['pm25'])
+            aqiDateTime = str(measurement['time'])
 
-    #Proper way to extract from json file by reading json structure
-    with open("/var/www/html/aqi.json", "r") as read_file:
-        measurementdata = json.load(read_file)
-        measurement = measurementdata[-1]
-        #print(measurement)
-        aqipm10 = "PM10: " + str(measurement['pm10'])
-        aqipm25 = "PM2.5: " + str(measurement['pm25'])
-        aqiDateTime = str(measurement['time'])
+        aqiHeader = "Air Quality Sensor"
+
+        print(aqiHeader)
+        print(aqiDateTime)
+        print(aqipm25)
+        print(aqipm10)
+
+        # Write four lines of text.
+        draw.text((x, top+0), aqiHeader, font=font, fill=255)
+        draw.text((x, top+8), aqiDateTime, font=font, fill=255)
+        draw.text((x, top+16), aqipm25, font=font, fill=255)
+        draw.text((x, top+25), aqipm10, font=font, fill=255)
+
+        # Display image.
+        disp.image(image)
+        disp.show()
+        
+    except:
+        e = sys.exc_info()[0]
+        print("Exception %s" % e)
+
+if __name__ == "__main__":
+
+    patterns = ["*.json"]
+    ignore_patterns = ""
+    ignore_directories = False
+    case_sensitive = True
+    my_event_handler = PatternMatchingEventHandler(patterns, ignore_patterns, ignore_directories, case_sensitive)
+
+    my_event_handler.on_created = on_created
+    my_event_handler.on_deleted = on_deleted
+    my_event_handler.on_modified = on_modified
+    my_event_handler.on_moved = on_moved
+
+
+    path = "/var/www/html"
+    go_recursively = True
+    my_observer = Observer()
+    my_observer.schedule(my_event_handler, path, recursive=go_recursively)
+
+    # Create the I2C interface.
+    i2c = busio.I2C(SCL, SDA)
     
-    aqiHeader = "Air Quality Sensor"
-    # Write four lines of text.
- 
-    #draw.text((x, top+0), "IP: "+IP, font=font, fill=255)
-    #draw.text((x, top+8), CPU, font=font, fill=255)
-    #draw.text((x, top+16), MemUsage, font=font, fill=255)
-    #draw.text((x, top+25), Disk, font=font, fill=255)
-
-    draw.text((x, top+0), aqiHeader, font=font, fill=255)
-    draw.text((x, top+8), aqiDateTime, font=font, fill=255)
-    draw.text((x, top+16), aqipm10, font=font, fill=255)
-    draw.text((x, top+25), aqipm25, font=font, fill=255)
- 
-    # Display image.
-    disp.image(image)
+    # Create the SSD1306 OLED class.
+    # The first two parameters are the pixel width and pixel height.  Change these
+    # to the right size for your display!
+    disp = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c)
+    
+    # Clear display.
+    disp.fill(0)
     disp.show()
-    #time.sleep(.1)
-    time.sleep(1)
     
+    # Create blank image for drawing.
+    # Make sure to create image with mode '1' for 1-bit color.
+    width = disp.width
+    height = disp.height
+    image = Image.new('1', (width, height))
+    
+    # Get drawing object to draw on image.
+    draw = ImageDraw.Draw(image)
+    
+    # Draw a black filled box to clear the image.
+    draw.rectangle((0, 0, width, height), outline=0, fill=0)
+    
+    # Draw some shapes.
+    # First define some constants to allow easy resizing of shapes.
+    padding = -2
+    top = padding
+    bottom = height-padding
+    # Move left to right keeping track of the current x position for drawing shapes.
+    x = 0
+    
+    
+    # Load default font.
+    font = ImageFont.load_default()
+    
+    # Alternatively load a TTF font.  Make sure the .ttf font file is in the
+    # same directory as the python script!
+    # Some other nice fonts to try: http://www.dafont.com/bitmap.php
+    #font = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf', 9)
+
+    #function to clear the screen on exit
+    def disableDisplay():
+        disp.poweroff()
+    atexit.register(disableDisplay)
+
+    # draw file content up front
+    redraw()
+    my_observer.start()
+    try:
+        while True:
+            #print("waiting for something to happen")
+            time.sleep(1)
+    except KeyboardInterrupt:
+        my_observer.stop()
+        my_observer.join()
