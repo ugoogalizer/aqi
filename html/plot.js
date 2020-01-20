6690d7a3027545321d8ab63fadc9bc7bffd0c51f
ugoogalizer <signup@mattcurtis.id.au>
2020-01-20 21:08:25 +1100

    Tidying up bar graph

--- html/plot.js
+++ html/plot.js
@@ -6,26 +6,27 @@ xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         var myArr = JSON.parse(this.responseText);
         PlotGraph(myArr);
+        PlotGraph2(myArr);
     }
 };
 
 xmlhttp.open("GET", "aqi.json", true);
 xmlhttp.send();
 
-
-
 function PlotGraph(data) {
   let trace1 = {
     x: [],
     y: [],
     //mode: "lines"
-    type: "scatter"
+    type: "scatter",
+    name: "PM2.5"
   };
   let trace2 = {
     x: [],
     y: [],
     //mode: "lines"
-    type: "scatter"
+    type: "scatter",
+    name: "PM10"
   };
     data.forEach(function(val) {
       var entryDateTime = new Date(val.time);
@@ -38,4 +39,24 @@ function PlotGraph(data) {
       }
     });
   Plotly.newPlot('AQIplot', [trace1, trace2]);
+}; 
+
+
+function PlotGraph2(data) {
+  let trace1 = {
+    x: [],
+    y: [],
+    //mode: "lines"
+    type: "bar",
+    name: "PM2.5"
+  };
+    data.forEach(function(val) {
+      var entryDateTime = new Date(val.time);
+      if (entryDateTime >= (Date.now() - 24*60*60*1000)) {
+        trace1.x.push(entryDateTime);
+        trace1.y.push(val["pm25"]);
+      
+      }
+    });
+  Plotly.newPlot('AQIplotBarpm25', [trace1, trace2]);
 }; 
